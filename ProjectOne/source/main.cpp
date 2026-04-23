78e1e256965e3d60edadf6edae798e8798c8b366
Georgi Zlatev <gthebox@gmail.com>
2026-04-23 19:10:26 +0300

    add: char

--- ProjectOne/source/main.cpp
+++ ProjectOne/source/main.cpp
@@ -12,39 +12,26 @@ extern "C" {
 		#include "cpp/functions.hpp"
 #endif
 
-////c
-//int main()
-//{
-//	int rows = 10;
-//	int columns = 10;
-//	int fill = 6;
-//
-//	while (true) {
-//		int** arr = new_2d_array(rows, columns, fill);
-//		fill_2d_array(arr, rows, columns, fill);
-//		print_2d_array(arr, rows, columns);
-//		fill_2d_array(arr, rows, columns, 3);
-//		print_2d_array(arr, rows, columns);
-//		delete_2d_array(arr, rows);
-//	}
-//}
-
-// cpp
-
+//#include <string> // cpp
+//#include <cstring> // c
 
 int main()
 {
-	int rows = 10;
-	int columns = 10;
-	int fill = 6;
+	//Value intialization
+	int a{ 1 };
 
-	while (true) {
-		std::vector<std::vector<int>> matrix;
+	//Direct intialization
+	int b(2);
 
-		Create2DMatrix(matrix, rows, columns, fill);
-		Print2DMatrix(matrix);
-		Fill2DMatrix(matrix, -3);
-		Print2DMatrix(matrix);
-		Delete2DMatrix(matrix);
-	}
+	//Copy intialization
+	int c = 3;
+
+	//Reference intialization
+	int& d = a;
+
+	//Direct List intialization
+	int e[]{ 1,2,3 };
+
+	//Copy list intialization
+	int f[] = { 1,2,3 };
 }
